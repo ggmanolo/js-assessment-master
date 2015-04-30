fb4c9824d42c2001e290d92c645d8a96f243e195
Manuel Garcia Genta <man.mdp@gmail.com>
2015-04-30 16:52:30 -0300

    Arrays practice in progress

--- app/arrays.js
+++ app/arrays.js
@@ -2,40 +2,71 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 
 define(function() {
   return {
-    indexOf : function(arr, item) {
 
+    indexOf : function(arr, item) {
+      for(var i=0; i < arr.length; i++) {
+        if(arr[i] === item) {
+          return i;
+        }
+      }
+      return -1;
     },
 
     sum : function(arr) {
-
+      var suma = 0;
+      for(var i=0; i < arr.length; i++) {
+        suma += arr[i];
+      }
+      return suma;
     },
 
     remove : function(arr, item) {
-
+      var aux = [];
+      for(var i=0; i < arr.length; i++) {
+        if(arr[i] !== item) {
+          aux.push(arr[i]);
+        }
+      }
+      return aux;
     },
 
     removeWithoutCopy : function(arr, item) {
-
+      var i,
+          len = arr.length;
+      for(i=0; i<len; i++) {
+        if(arr[i] === item) {
+          arr.splice(i, 1);
+          i -= 1;
+          len -= 1;
+        }
+      }
+      return arr;
     },
 
     append : function(arr, item) {
-
+      arr.push(item);
+      return arr;
     },
 
     truncate : function(arr) {
-
+      arr.pop();
+      return arr;
     },
 
     prepend : function(arr, item) {
-
+      arr.unshift(item);
+      return arr;
     },
 
     curtail : function(arr) {
-
+      arr.shift();
+      return arr;
     },
 
     concat : function(arr1, arr2) {
-
+      var aux;
+      aux = arr1.concat(arr2);
+      return aux;
     },
 
     insert : function(arr, item, index) {
--- app/objects.js
+++ app/objects.js
@@ -3,7 +3,7 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     alterContext : function(fn, obj) {
-
+      return fn.apply(obj);
     },
 
     alterObjects : function(constructor, greeting) {
